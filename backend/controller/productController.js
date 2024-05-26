68e3dc1c52c0f6b26a9a971c4439c931233cef85
=Punit2709 <=punitsp2003@gmail.com>
2024-05-26 16:45:26 +0530

    Product Routes Added with Try Catch

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,16 +1,74 @@
-const Product = require('../models/productModel');
+const Product = require("../models/productModel");
 
-// creating Product 
-exports.createProduct = async (req, res, next) =>{ 
-    const product =  await Product.create(req.body);
-    res.status(200).json({message: 'Product Created', success: true, product});
-}
-
-
-// get products
-exports.getAllProducts = (req, res) =>{
-    res.status(200).json({message : 'route is working fine'})
+// creating Product : Admin
+exports.createProduct = async (req, res, next) => {
+  try {
+    const product = await Product.create(req.body);
+    res
+      .status(200)
+      .json({ message: "Product Created", success: true, product });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
 };
 
+// update product : Admin
+exports.updateProduct = async (req, res, next) => {
+  try {
+    let product = await Product.findById(req.params.id);
 
+    if (!product) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Product not Folund" });
+    }
 
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res
+      .status(200)
+      .json({ success: true, message: "Product Updated", product });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+// delete product : Admin
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    await product.remove();
+    res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+// get products
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res
+      .status(200)
+      .json({ message: "Product Created", success: true, products });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
--- backend/routers/productRoute.js
+++ backend/routers/productRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getAllProducts, createProduct } = require('../controller/productController');
+const { getAllProducts, createProduct, updateProduct, deleteProduct } = require('../controller/productController');
 
 const router = express.Router();
 
 router.route('/products').get(getAllProducts);
 router.route('/products/create').post(createProduct);
+router.route('/products/update/:id').put(updateProduct);
+router.route('/products/delete/:id').delete(deleteProduct);
 
 module.exports = router;
 
