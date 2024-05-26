3a0989aa11608f5acd771f75375331241d2679be
=Punit2709 <=punitsp2003@gmail.com>
2024-05-26 13:02:24 +0530

    Product create route created

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,4 +1,16 @@
+const Product = require('../models/productModel');
+
+// creating Product 
+exports.createProduct = async (req, res, next) =>{ 
+    const product =  await Product.create(req.body);
+    res.status(200).json({message: 'Product Created', success: true, product});
+}
+
+
+// get products
 exports.getAllProducts = (req, res) =>{
     res.status(200).json({message : 'route is working fine'})
 };
+
+
 
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -46,11 +46,11 @@ const ProductSchema = mongoose.Schema({
     },
     reviews: [
         {
-            user: {
-                type: mongoose.Schema.ObjectId,
-                ref: "User",
-                required: true,
-            },
+            // user: {
+            //     type: mongoose.Schema.ObjectId,
+            //     ref: "User",
+            //     required: true,
+            // },
             name:{
                 type: String,
                 required: true,
