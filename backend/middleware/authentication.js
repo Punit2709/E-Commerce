9154673f292f560bfdea404d4ae16f1334459f5b
=Punit2709 <=punitsp2003@gmail.com>
2024-05-27 22:26:16 +0530

    Authorize(Admin) user role Access

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -1,22 +1,39 @@
 const catchAsyncError = require("../middleware/catchAsyncError");
 const ErrorHandler = require("../utils/errorHandler");
 const jwt = require("jsonwebtoken");
-const User = require('../models/userModel')
+const User = require("../models/userModel");
 
-const isAuthenticatedUser = catchAsyncError( async (req, res, next) => {
-    const { token } = req.cookies;
+const isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
 
-    if(!token){
-        return next(new ErrorHandler(401, 'Please Login for Access the Resource'));
+  if (!token) {
+    return next(new ErrorHandler(401, "Please Login for Access the Resource"));
+  }
+
+  // console.log(token);
+
+  // taking id which passed for making JWT data
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await User.findById(decodedData.id);
+
+  next();
+});
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    console.log(req.user.role)
+    console.log(roles);
+    console.log(roles.includes(req.user.role));
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          401,
+          `${req.user.role} is not allow to access this resource`
+        )
+      );
     }
-
-    // console.log(token);
-
-    // taking id which passed for making JWT data
-    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decodedData.id);
-
     next();
-}); 
+  };
+};
 
-module.exports = isAuthenticatedUser;
+module.exports = { isAuthenticatedUser, authorizeRoles };
--- backend/routers/productRoute.js
+++ backend/routers/productRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require('../controller/productController');
-const isAuthenticatedUser = require('../middleware/authentication');
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/authentication');
 
 const router = express.Router();
 
 router.route('/products').get(getAllProducts);
-router.route('/products/create').post( isAuthenticatedUser, createProduct);
-router.route('/products/update/:id').put( isAuthenticatedUser, updateProduct);
-router.route('/products/delete/:id').delete( isAuthenticatedUser, deleteProduct);
+router.route('/products/create').post( isAuthenticatedUser, authorizeRoles('admin'),  createProduct);
+router.route('/products/update/:id').put( isAuthenticatedUser, authorizeRoles('admin'), updateProduct);
+router.route('/products/delete/:id').delete( isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 router.route('/products/details/:id').get(getProductDetails);
 
 module.exports = router;
