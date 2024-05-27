8ca8f375c9e84cbb753f1ce789f9bdfc4267ddcd
=Punit2709 <=punitsp2003@gmail.com>
2024-05-27 18:28:46 +0530

    Logout Route is Implemented

--- backend/routers/productRoute.js
+++ backend/routers/productRoute.js
@@ -4,10 +4,10 @@ const isAuthenticatedUser = require('../middleware/authentication');
 
 const router = express.Router();
 
-router.route('/products').get( isAuthenticatedUser, getAllProducts);
-router.route('/products/create').post(createProduct);
-router.route('/products/update/:id').put(updateProduct);
-router.route('/products/delete/:id').delete(deleteProduct);
+router.route('/products').get(getAllProducts);
+router.route('/products/create').post( isAuthenticatedUser, createProduct);
+router.route('/products/update/:id').put( isAuthenticatedUser, updateProduct);
+router.route('/products/delete/:id').delete( isAuthenticatedUser, deleteProduct);
 router.route('/products/details/:id').get(getProductDetails);
 
 module.exports = router;
