af279ad572b2e20a76479980338cb1ed8510d292
=Punit2709 <=punitsp2003@gmail.com>
2024-05-27 18:26:03 +0530

    Logout Route is Implemented

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -45,3 +45,18 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
 
   sendJWTToken(user,200, 'User Logged In', res);
 });
+
+
+exports.logoutUser = catchAsyncError(async (req, res, next) => {
+
+  res.cookie('token', null, {
+    expires: new Date(Date.now()),
+    httpOnly : true
+  })
+
+  res.status(200).json({
+    success: true,
+    message: 'User Logged Out Successfully', 
+  });
+
+});
