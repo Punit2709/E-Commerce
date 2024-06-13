d1fc5764b13d614856049ce04a331f29c514694c
=Punit2709 <=punitsp2003@gmail.com>
2024-06-13 12:48:22 +0530

    Working On Deployment

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-
+const path = require("path");
+const cors = require("cors");
 
 const dotenv = require('dotenv');
 dotenv.config({path: 'backend/config/config.env'});
@@ -27,12 +28,19 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(cors());
 
 
 app.use('/api/v1', product);
 app.use('/api/v1', user);
 app.use('/api/v1', order);
 app.use('/api/v1', payment);
+
+app.use(express.static(path.join(__dirname, "../Frontend/public")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../Frontend/public/index.html"));
+  });
 
 app.use(errorMiddleware);
 module.exports = app;
